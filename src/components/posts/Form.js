bb5f5c850c188508d0b8bf4c5932256ca9d9bcbc
PGVlasov <PGVlasov@users.noreply.github.com>
2022-05-24 20:57:10 +0300

    added email validation

--- src/components/posts/Form.js
+++ src/components/posts/Form.js
@@ -1,29 +1,40 @@
 import { useState } from "react";
 import axios from "axios";
 import "./Post.css";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 export const Form = () => {
   const [commentInput, setCommentInput] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [validate, setValidate] = useState(false);
+
+  const navigate = useNavigate("");
+
+  const validateEmail = (email) => {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
 
   const param = useParams();
 
   const postId = parseInt(param.postId, 10);
+  const userId = parseInt(param.userId, 10);
 
   const changeNameHandler = (event) => {
     setName(event.target.value);
   };
   const changeEmailHandler = (event) => {
+    if (validateEmail(email)) {
+      setValidate(true);
+    }
     setEmail(event.target.value);
   };
   const changeContentHandler = (event) => {
     setCommentInput(event.target.value);
   };
 
-  const addComment = (event) => {
-    event.preventDefault();
+  const addComment = () => {
     setCommentInput("");
     setName("");
     setEmail("");
@@ -40,6 +51,7 @@ export const Form = () => {
       .catch(function (error) {
         console.log(error);
       });
+    navigate(`/users/${userId}/posts/${postId}`);
   };
 
   return (
@@ -47,7 +59,7 @@ export const Form = () => {
       <form className="form">
         <div className="inputContainer">
           <input
-            className="input"
+            className={"input"}
             type="text"
             id="title"
             placeholder="Вашe Имя"
@@ -55,13 +67,14 @@ export const Form = () => {
             onChange={changeNameHandler}
           />
           <input
-            className="input"
-            type="text"
+            type="email"
+            className={validate ? "input" : "input-wrong"}
             id="title"
             placeholder="Ваш Еmail"
             value={email}
             onChange={changeEmailHandler}
           />
+          <label>{!validate ? <p>введите корректный email</p> : <p></p>}</label>
         </div>
         <textarea
           className="textarea"
@@ -72,9 +85,13 @@ export const Form = () => {
           onChange={changeContentHandler}
         />
         <button
-          className="postButton"
+          className={
+            !commentInput || !name || !email || !validate
+              ? "postButton-disabled"
+              : "postButton"
+          }
           onClick={(event) => addComment(event)}
-          disabled={!commentInput || !name || !email}
+          disabled={!commentInput || !name || !email || !validate}
         >
           <p className="buttonp">Отправить</p>
         </button>
